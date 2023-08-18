e7b7178afe73fd9b5ba3d391c95230bd532dd86c
Aswin Karki <aswin.karki44@gmail.com>
2023-08-18 14:41:13 +0545

    added Card component and some dummy data

--- App.tsx
+++ App.tsx
@@ -1,20 +1,133 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Constants from "expo-constants";
+import { Dimensions, Image, StyleSheet, Text, View } from "react-native";
+
+import { Entypo } from "@expo/vector-icons";
+import { StatusBar } from "expo-status-bar";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import data, { locationImage } from "./data";
+
+const { width } = Dimensions.get("window");
+const duration = 300;
+const _size = width * 0.9;
+const layout = {
+  borderRadius: 16,
+  width: _size,
+  height: _size * 1.27,
+  spacing: 12,
+  cardsGap: 22,
+};
+const maxVisibleItems = 6;
+
+const colors = {
+  primary: "#6667AB",
+  light: "#fff",
+  dark: "#111",
+};
+
+function Card({
+  info,
+  index,
+  totalLength,
+}: {
+  totalLength: number;
+  index: number;
+  info: (typeof data)[0];
+}) {
+  return (
+    <View style={[styles.card]}>
+      <Text
+        style={[
+          styles.title,
+          {
+            position: "absolute",
+            top: -layout.spacing,
+            right: layout.spacing,
+            fontSize: 102,
+            color: colors.primary,
+            opacity: 0.05,
+          },
+        ]}
+      >
+        {index}
+      </Text>
+      <View style={styles.cardContent}>
+        <Text style={styles.title}>{info.type}</Text>
+        <View style={styles.row}>
+          <Entypo name="clock" size={16} style={styles.icon} />
+          <Text style={styles.subtitle}>
+            {info.from} - {info.to}
+          </Text>
+        </View>
+        <View style={styles.row}>
+          <Entypo name="location" size={16} style={styles.icon} />
+          <Text style={styles.subtitle}>{info.distance} km</Text>
+        </View>
+        <View style={styles.row}>
+          <Entypo name="suitcase" size={16} style={styles.icon} />
+          <Text style={styles.subtitle}>{info.role}</Text>
+        </View>
+      </View>
+      <Image source={{ uri: locationImage }} style={styles.locationImage} />
+    </View>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GestureHandlerRootView style={styles.container}>
+      <StatusBar hidden />
+      <View
+        style={{
+          alignItems: "center",
+          flex: 1,
+          justifyContent: "flex-end",
+          marginBottom: layout.cardsGap * 2,
+        }}
+        pointerEvents="box-none"
+      >
+        {data.slice(0, 1).map((c, index) => {
+          return (
+            <Card
+              info={c}
+              key={c.id}
+              index={index}
+              totalLength={data.length - 1}
+            />
+          );
+        })}
+      </View>
+    </GestureHandlerRootView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: colors.primary,
+    padding: layout.spacing,
   },
+  card: {
+    borderRadius: layout.borderRadius,
+    width: layout.width,
+    height: layout.height,
+    padding: layout.spacing,
+    backgroundColor: colors.light,
+  },
+  title: { fontSize: 32, fontWeight: "600" },
+  subtitle: {},
+  cardContent: {
+    gap: layout.spacing,
+    marginBottom: layout.spacing,
+  },
+  locationImage: {
+    flex: 1,
+    borderRadius: layout.borderRadius - layout.spacing / 2,
+  },
+  row: {
+    flexDirection: "row",
+    columnGap: layout.spacing / 2,
+    alignItems: "center",
+  },
+  icon: {},
 });
